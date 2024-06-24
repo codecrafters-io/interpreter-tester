e3536def3edfc0448b732339929d757911fabd94
Ryan Gang <ryan-gg@outlook.com>
2024-06-25 00:37:10 +0530

    feat: refactor stage1 to use tester framework

--- internal/stage1.go
+++ internal/stage1.go
@@ -1,11 +1,8 @@
 package internal
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/codecrafters-io/grep-tester/internal/assertions"
 	"github.com/codecrafters-io/grep-tester/internal/interpreter_executable"
+	testcases "github.com/codecrafters-io/grep-tester/internal/test_cases"
 
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
@@ -15,44 +12,61 @@ func testInit(stageHarness *test_case_harness.TestCaseHarness) error {
 
 	logger := stageHarness.Logger
 
-	tmpFileName, err := createTempFileWithContents("(())##")
-	if err != nil {
-		return err
+	logger.Infof("\nRunning test: 1")
+	commandTestCase1 := testcases.ReceiveOutputTestCase{
+		FileContents:   "(",
+		Command:        "tokenize",
+		ExpectedStdout: []string{"LEFT_PAREN ( null", "EOF  null"},
+		ExpectedStderr: []string{},
 	}
-	defer os.Remove(tmpFileName)
-
-	result, err := b.Run("tokenize", tmpFileName)
-	if err != nil {
-		return err
-	}
-
-	if result.ExitCode != 0 {
-		return fmt.Errorf("expected exit code %v, got %v", 0, result.ExitCode)
-	}
-
-	stdOut := getOutputFromStdOut(result)
-	stdErr := getOutputFromStdErr(result)
-
-	expectedStdout := []string{"LEFT_PAREN ( null", "LEFT_PAREN ( null", "RIGHT_PAREN ) null", "RIGHT_PAREN ) null", "EOF  null"}
-
-	expectedStderr := []string{"[line 1] Error : Unexpected character: #", "[line 1] Error : Unexpected character: #"}
-
-	assertionResult, err := assertions.NewOrderedStringArrayAssertion(expectedStdout, "stdout").Run(stdOut)
-	for _, line := range assertionResult {
-		logger.Successf(line)
-	}
-	if err != nil {
+	if err := commandTestCase1.Run(b, logger); err != nil {
 		return err
 	}
 
-	assertionResult, err = assertions.NewOrderedStringArrayAssertion(expectedStderr, "stderr").Run(stdErr)
-	for _, line := range assertionResult {
-		logger.Successf(line)
+	logger.Infof("\nRunning test: 2")
+	commandTestCase2 := testcases.ReceiveOutputTestCase{
+		FileContents:   "))",
+		Command:        "tokenize",
+		ExpectedStdout: []string{"RIGHT_PAREN ) null", "RIGHT_PAREN ) null", "EOF  null"},
+		ExpectedStderr: []string{},
 	}
-	if err != nil {
+	if err := commandTestCase2.Run(b, logger); err != nil {
 		return err
 	}
 
-	logger.Successf("✓ Received exit code %d.", 0)
+	logger.Infof("\nRunning test: 3")
+	commandTestCase3 := testcases.ReceiveOutputTestCase{
+		FileContents:   "(())",
+		Command:        "tokenize",
+		ExpectedStdout: []string{"LEFT_PAREN ( null", "LEFT_PAREN ( null", "RIGHT_PAREN ) null", "RIGHT_PAREN ) null", "EOF  null"},
+		ExpectedStderr: []string{},
+	}
+	if err := commandTestCase3.Run(b, logger); err != nil {
+		return err
+	}
+
+	logger.Infof("\nRunning test: 4")
+	commandTestCase4 := testcases.ReceiveOutputTestCase{
+		FileContents:   ")(())()(())(",
+		Command:        "tokenize",
+		ExpectedStdout: []string{"RIGHT_PAREN ) null", "LEFT_PAREN ( null", "LEFT_PAREN ( null", "RIGHT_PAREN ) null", "RIGHT_PAREN ) null", "LEFT_PAREN ( null", "RIGHT_PAREN ) null", "LEFT_PAREN ( null", "LEFT_PAREN ( null", "RIGHT_PAREN ) null", "RIGHT_PAREN ) null", "LEFT_PAREN ( null", "EOF  null"},
+		ExpectedStderr: []string{},
+	}
+	if err := commandTestCase4.Run(b, logger); err != nil {
+		return err
+	}
+
+	// ToDo: No need to test errors here
+	logger.Infof("\nRunning test: X")
+	commandTestCaseX := testcases.ReceiveOutputTestCase{
+		FileContents:   "(())##",
+		Command:        "tokenize",
+		ExpectedStdout: []string{"LEFT_PAREN ( null", "LEFT_PAREN ( null", "RIGHT_PAREN ) null", "RIGHT_PAREN ) null", "EOF  null"},
+		ExpectedStderr: []string{"[line 1] Error: Unexpected character: #", "[line 1] Error: Unexpected character: #"},
+	}
+	if err := commandTestCaseX.Run(b, logger); err != nil {
+		return err
+	}
+
 	return nil
 }
