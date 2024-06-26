f82a36b77f303495e0a88231759e274742f738a8
Ryan Gang <ryan-gg@outlook.com>
2024-06-27 00:32:18 +0530

    feat: add random string values for stage 4

--- internal/stage4.go
+++ internal/stage4.go
@@ -4,6 +4,7 @@ import (
 	"github.com/codecrafters-io/grep-tester/internal/interpreter_executable"
 	testcases "github.com/codecrafters-io/grep-tester/internal/test_cases"
 
+	"github.com/codecrafters-io/tester-utils/random"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
@@ -12,10 +13,10 @@ func testSingleChars(stageHarness *test_case_harness.TestCaseHarness) error {
 
 	logger := stageHarness.Logger
 
-	// shuffledString1 := random.RandomStringFromCharacters(15, []rune("+-*.,;"))
-	// shuffledString2 := random.RandomStringFromCharacters(20, []rune("+-*.,;(){}"))
+	shuffledString1 := random.RandomStringFromCharacters(15, []rune("+-*.,;"))
+	shuffledString2 := random.RandomStringFromCharacters(20, []rune("+-*.,;(){}"))
 	commandTestCases := testcases.MultiTokenizeTestCase{
-		FileContents: []string{"+-", "+-*.,;"},
+		FileContents: []string{"+-", "+-*.,;", ".;++-+,-++.**,;", shuffledString1, shuffledString2},
 	}
 	if err := commandTestCases.RunAll(b, logger); err != nil {
 		return err
