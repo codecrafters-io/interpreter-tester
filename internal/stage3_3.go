032dc43d3caec6dad287ea19e3a7ef35962f7c66
Ryan Gang <ryan-gg@outlook.com>
2024-06-26 14:28:58 +0530

    feat: add test case for multiple tokenize_output_test_cases Further cutting down on the amount of boiler plate required for each test

---
+++ internal/stage3_3.go
@@ -0,0 +1,27 @@
+package internal
+
+import (
+	"github.com/codecrafters-io/grep-tester/internal/interpreter_executable"
+	testcases "github.com/codecrafters-io/grep-tester/internal/test_cases"
+
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testParen(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := interpreter_executable.NewInterpreterExecutable(stageHarness)
+
+	logger := stageHarness.Logger
+
+	shuffledString1 := random.RandShuffledStr(5, []rune("()"))
+	shuffledString2 := random.RandShuffledStr(25, []rune("()"))
+	shuffledString3 := random.RandShuffledStr(5, []rune("()")) + "##"
+	commandTestCases := testcases.MultiTokenizeTestCase{
+		FileContents: []string{"(", "((", shuffledString1, shuffledString2, shuffledString3},
+	}
+	if err := commandTestCases.RunAll(b, logger); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/test_cases/multi_tokenize_test_case.go
@@ -0,0 +1,25 @@
+package testcases
+
+import (
+	"github.com/codecrafters-io/grep-tester/internal/interpreter_executable"
+	"github.com/codecrafters-io/tester-utils/logger"
+)
+
+// ToDo: Improve test case name
+
+type MultiTokenizeTestCase struct {
+	FileContents []string
+}
+
+func (t *MultiTokenizeTestCase) RunAll(executable *interpreter_executable.InterpreterExecutable, logger *logger.Logger) error {
+	for i, fileContents := range t.FileContents {
+		logger.Infof("Running test case: %d", i+1)
+		testCase := TokenizeOutputTestCase{
+			FileContents: fileContents}
+		if err := testCase.Run(executable, logger); err != nil {
+			return err
+		}
+		logger.Infof("Test case %d passed\n", i+1)
+	}
+	return nil
+}
