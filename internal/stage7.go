cb59916f6110b0e2a5c416c661dcc5c109d4d098
Ryan Gang <ryan-gg@outlook.com>
2024-06-27 14:38:13 +0530

    feat: implement test for stage_7

---
+++ internal/stage7.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"github.com/codecrafters-io/grep-tester/internal/interpreter_executable"
+	testcases "github.com/codecrafters-io/grep-tester/internal/test_cases"
+
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testNegation(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := interpreter_executable.NewInterpreterExecutable(stageHarness)
+
+	logger := stageHarness.Logger
+
+	shuffledString1 := random.RandomStringFromCharacters(20, []rune("!!==@"))
+	commandTestCases := testcases.MultiTokenizeTestCase{
+		FileContents: []string{"!=", "!!===", "(!)#(!===)", shuffledString1},
+	}
+	if err := commandTestCases.RunAll(b, logger); err != nil {
+		return err
+	}
+
+	return nil
+}
