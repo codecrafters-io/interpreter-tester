0f302c4b7a9c41344abac8020526f2975113a926
Ryan Gang <ryan-gg@outlook.com>
2024-07-01 01:51:35 +0530

    feat: implement test for stage_9

---
+++ internal/stage9.go
@@ -0,0 +1,34 @@
+package internal
+
+import (
+	"slices"
+
+	"github.com/codecrafters-io/grep-tester/internal/interpreter_executable"
+	testcases "github.com/codecrafters-io/grep-tester/internal/test_cases"
+
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+var Division = []string{"/"}
+
+func testComments(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := interpreter_executable.NewInterpreterExecutable(stageHarness)
+
+	logger := stageHarness.Logger
+
+	// As whitespace is not introduced yet, we skip multi-line comments here.
+	comment1 := "//Comment"
+	comment2 := "()//Comment"
+	comment3 := "//Unicode:s£§¶ÜÞĐĦŋœƂƢƩǁឃᢆ᯽₪ℜ↩⊗┺░☃☺♣"
+	division1 := "/"
+	division2 := random.RandomStringFromCharacters(20, slices.Concat(Parens, Braces, SingleCharOperators, LexicalErrors, Equals, Negation, Relational)) + "//Comment"
+	commandTestCases := testcases.MultiTokenizeTestCase{
+		FileContents: []string{comment1, comment2, comment3, division1, division2},
+	}
+	if err := commandTestCases.RunAll(b, logger); err != nil {
+		return err
+	}
+
+	return nil
+}
