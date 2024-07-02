2abcafb3923d5c73ec14737d6bb7d05d92c10e34
Ryan Gang <ryan-gg@outlook.com>
2024-07-02 15:10:41 +0530

    refactor: Update stage12.go to use escaped string literals

--- internal/stage12.go
+++ internal/stage12.go
@@ -6,22 +6,21 @@ import (
 	"github.com/codecrafters-io/grep-tester/internal/interpreter_executable"
 	testcases "github.com/codecrafters-io/grep-tester/internal/test_cases"
 
-	"github.com/codecrafters-io/tester-utils/random"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
-var Strings = []string{"hello", "world", "foo", "bar", "baz"}
+var Strings = []string{"\"hello\"", "\"world\"", "\"foo\"", "\"bar\"", "\"baz\""}
 
 func testStrings(stageHarness *test_case_harness.TestCaseHarness) error {
 	b := interpreter_executable.NewInterpreterExecutable(stageHarness)
 
 	logger := stageHarness.Logger
 
-	shuffledString1 := random.RandomStringFromCharacters(20, slices.Concat(Whitespace, Strings))
-	shuffledString2 := random.RandomStringFromCharacters(35, slices.Concat(Parens, Braces, SingleCharOperators, LexicalErrors, Equals, Negation, Relational, Division, Whitespace, Strings))
+	shuffledString1 := randomStringFromCharactersNew(15, slices.Concat(Whitespace, Strings))
+	shuffledString2 := randomStringFromCharactersNew(25, slices.Concat(Parens, Braces, SingleCharOperators, LexicalErrors, Equals, Negation, Relational, Division, Whitespace, Strings))
 
 	commandTestCases := testcases.MultiTokenizeTestCase{
-		FileContents: []string{"", ` foo	baz `, shuffledString1, shuffledString2},
+		FileContents: []string{Strings[0], ` "foo"			"baz" `, shuffledString1, shuffledString2},
 	}
 	if err := commandTestCases.RunAll(b, logger); err != nil {
 		return err
