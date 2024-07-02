edcc65aefe3b7751bfb991c40127e2e11f1f379b
Ryan Gang <ryan-gg@outlook.com>
2024-07-02 15:13:08 +0530

    feat: update float representation from Scanner API

--- internal/lox/scanner_api.go
+++ internal/lox/scanner_api.go
@@ -1,6 +1,10 @@
 package lox
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 func ScanTokens(source string) ([]string, []string, int, error) {
 	scanner := NewScanner(source)
@@ -14,6 +18,8 @@ func ScanTokens(source string) ([]string, []string, int, error) {
 		literal := token.Literal
 		if literal == nil {
 			literal = "null"
+		} else if reflect.TypeOf(literal).Kind() == reflect.Float64 {
+			literal = formatFloat(literal.(float64))
 		}
 		tokenLines = append(tokenLines, fmt.Sprintf("%s %s %s", getTokenName(token.Type), token.Lexeme, literal))
 	}
@@ -23,4 +29,24 @@ func ScanTokens(source string) ([]string, []string, int, error) {
 	}
 
 	return tokenLines, errorLines, exitCode, nil
+}
+
+func formatFloat(num float64) string {
+	str := fmt.Sprintf("%f", num)
+	parts := strings.Split(str, ".")
+	if len(parts) != 2 {
+		return str
+	}
+	integerPart := parts[0]
+	decimalPart := parts[1]
+
+	// Remove trailing zeros from the decimal part
+	decimalPart = strings.TrimRight(decimalPart, "0")
+
+	// Ensure at least one decimal place
+	if decimalPart == "" {
+		decimalPart = "0"
+	}
+
+	return integerPart + "." + decimalPart
 }
