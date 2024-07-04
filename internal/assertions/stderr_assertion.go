137aa30fafffe8ec2dd1ed1db533abf16146c8a9
Ryan Gang <ryan-gg@outlook.com>
2024-07-04 22:50:55 +0530

    feat: improve error handling when number of lines mismatch between expected and actual outputs

--- internal/assertions/stderr_assertion.go
+++ internal/assertions/stderr_assertion.go
@@ -13,22 +13,30 @@ func NewStderrAssertion(expectedLines []string) StderrAssertion {
 }
 
 func (a StderrAssertion) Run(stderr []string) ([]string, error) {
-	if len(stderr) != len(a.ExpectedLines) {
-		return []string{}, fmt.Errorf("Expected %d lines of stderr, got %d", len(a.ExpectedLines), len(stderr))
-	}
-
 	var output []string
-	for i, expectedLines := range a.ExpectedLines {
+
+	for i, expectedLine := range a.ExpectedLines {
+		if i >= len(stderr) {
+			output = append(output, fmt.Sprintf("? %s", expectedLine))
+			return output, fmt.Errorf("Expected line #%d on stderr to be %q, but didn't find line", i+1, expectedLine)
+		}
 		actualValue := stderr[i]
 
-		if actualValue != expectedLines {
+		if actualValue != expectedLine {
 			output = append(output, fmt.Sprintf("𐄂 %s", actualValue))
-			return output, fmt.Errorf("Expected line #%d on stderr to be %q, got %q", i+1, expectedLines, actualValue)
+			return output, fmt.Errorf("Expected line #%d on stderr to be %q, got %q", i+1, expectedLine, actualValue)
 		} else {
 			output = append(output, fmt.Sprintf("✓ %s", actualValue))
 		}
 	}
 
+	if len(stderr) > len(a.ExpectedLines) {
+		output = append(output, fmt.Sprintf("! %s", stderr[len(a.ExpectedLines)]))
+		return output, fmt.Errorf("Expected last line to be %q, but found %d more line(s)", stderr[len(a.ExpectedLines)-1], len(stderr)-len(a.ExpectedLines))
+	}
+
+	// If all lines match, we don't want to print all the lines again
+	// We just want to print a single line summary
 	successOutput := []string{fmt.Sprintf("✓ %d line(s) match on stderr", len(a.ExpectedLines))}
 	return successOutput, nil
 }
--- internal/assertions/stdout_assertion.go
+++ internal/assertions/stdout_assertion.go
@@ -13,22 +13,30 @@ func NewStdoutAssertion(expectedLines []string) StdoutAssertion {
 }
 
 func (a StdoutAssertion) Run(stdout []string) ([]string, error) {
-	if len(stdout) != len(a.ExpectedLines) {
-		return []string{}, fmt.Errorf("Expected %d lines of stdout, got %d", len(a.ExpectedLines), len(stdout))
-	}
-
 	var output []string
-	for i, expectedLines := range a.ExpectedLines {
+
+	for i, expectedLine := range a.ExpectedLines {
+		if i >= len(stdout) {
+			output = append(output, fmt.Sprintf("? %s", expectedLine))
+			return output, fmt.Errorf("Expected line #%d on stdout to be %q, but didn't find line", i+1, expectedLine)
+		}
 		actualValue := stdout[i]
 
-		if actualValue != expectedLines {
+		if actualValue != expectedLine {
 			output = append(output, fmt.Sprintf("𐄂 %s", actualValue))
-			return output, fmt.Errorf("Expected line #%d on stdout to be %q, got %q", i+1, expectedLines, actualValue)
+			return output, fmt.Errorf("Expected line #%d on stdout to be %q, got %q", i+1, expectedLine, actualValue)
 		} else {
 			output = append(output, fmt.Sprintf("✓ %s", actualValue))
 		}
 	}
 
+	if len(stdout) > len(a.ExpectedLines) {
+		output = append(output, fmt.Sprintf("! %s", stdout[len(a.ExpectedLines)]))
+		return output, fmt.Errorf("Expected last line to be %q, but found %d more line(s)", stdout[len(a.ExpectedLines)-1], len(stdout)-len(a.ExpectedLines))
+	}
+
+	// If all lines match, we don't want to print all the lines again
+	// We just want to print a single line summary
 	successOutput := []string{fmt.Sprintf("✓ %d line(s) match on stdout", len(a.ExpectedLines))}
 	return successOutput, nil
 }
