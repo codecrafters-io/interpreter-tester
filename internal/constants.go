38263ad154207880782291951ea493f7b168a573
Ryan Gang <ryan-gg@outlook.com>
2024-07-12 12:09:16 +0530

    feat: add constants file

---
+++ internal/constants.go
@@ -0,0 +1,7 @@
+package internal
+
+var QUOTEDSTRINGS = []string{"\"hello\"", "\"world\"", "\"foo\"", "\"bar\"", "\"baz\""}
+
+var STRINGS = []string{"foo", "bar", "baz", "quz", "hello", "world"}
+
+var BOOLEANS = []string{"true", "false"}
