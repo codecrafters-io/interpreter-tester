51820a7da229e8b366e755adabae595000fea20c
Ryan Gang <ryan-gg@outlook.com>
2024-07-24 01:49:00 +0530

    refactor: add utility functions for generating random integers and strings

--- internal/utils.go
+++ internal/utils.go
@@ -1,9 +1,19 @@
 package internal
 
 import (
+	"fmt"
+
 	"github.com/codecrafters-io/tester-utils/random"
 )
 
 func getRandInt() int {
 	return random.RandomInt(10, 100)
+}
+
+func getRandIntAsString() string {
+	return fmt.Sprintf("%d", getRandInt())
+}
+
+func getRandString() string {
+	return random.RandomElementFromArray(STRINGS)
 }
