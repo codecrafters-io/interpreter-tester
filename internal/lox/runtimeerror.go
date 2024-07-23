f3f28002e5e35a4176d6c4a566af61be1261c8a5
Ryan Gang <ryan-gg@outlook.com>
2024-07-23 16:55:31 +0530

    feat: Add runtime error handling functionality

---
+++ internal/lox/runtimeerror.go
@@ -0,0 +1,20 @@
+package lox
+
+import (
+	"fmt"
+	"os"
+)
+
+// Print reports a runtime error
+func PrintRuntimeError(message string) {
+	fmt.Fprintf(os.Stderr, "%v\n", message)
+	HadRuntimeError = true
+}
+
+// MakeRuntimeError creates a new runtime error
+func MakeRuntimeError(token Token, message string) error {
+	return fmt.Errorf("[line %d]: %s\n", token.Line, message)
+}
+
+// HadRuntimeError is true if an evaluation error was encountered
+var HadRuntimeError = false
