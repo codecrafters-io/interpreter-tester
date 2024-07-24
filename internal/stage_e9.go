b85cdb74ca6c6305f6e6cf513f043bbb1bc91104
Ryan Gang <ryan-gg@outlook.com>
2024-07-24 17:29:46 +0530

    feat: add testEvaluateEquality function for evaluating equality expressions

---
+++ internal/stage_e9.go
@@ -0,0 +1,30 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/codecrafters-io/interpreter-tester/internal/interpreter_executable"
+	testcases "github.com/codecrafters-io/interpreter-tester/internal/test_cases"
+
+	"github.com/codecrafters-io/tester-utils/random"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func testEvaluateEquality(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := interpreter_executable.NewInterpreterExecutable(stageHarness)
+
+	logger := stageHarness.Logger
+
+	strings := random.RandomElementsFromArray(STRINGS, 2)
+	s1, s2 := strings[0], strings[1]
+	n1, n2 := getRandInt(), getRandInt()
+	equality1 := fmt.Sprintf("\"%s\" != \"%s\"", s1, s2)
+	equality2 := fmt.Sprintf("\"%s\" == \"%s\"", s1, s1)
+	equality3 := fmt.Sprintf("%d == \"%d\"", n1, n1)
+	equality4 := fmt.Sprintf("%d != \"%d\"", n2, n2)
+
+	evaluateTestCases := testcases.MultiEvaluateTestCase{
+		FileContents: []string{equality1, equality2, equality3, equality4},
+	}
+	return evaluateTestCases.RunAll(b, logger)
+}
