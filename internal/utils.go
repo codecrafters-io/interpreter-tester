2042a0f1e2c334d9a590ed89b1ae65bb3e17c867
Ryan Gang <ryan-gg@outlook.com>
2024-08-12 16:57:53 +0530

    feat: implement test program retrieval for current stage

--- internal/utils.go
+++ internal/utils.go
@@ -2,6 +2,10 @@ package internal
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/codecrafters-io/tester-utils/random"
 )
@@ -20,4 +24,36 @@ func getRandString() string {
 
 func getRandBoolean() string {
 	return random.RandomElementFromArray(BOOLEANS)
+}
+
+// From the "statements & state" extension onwards, we'll need to pass multi line programs for testing.
+// To make it easier to work with and track changes, all the test programs are stored in the test_programs directory.
+// Every stage has its own directory. (For example, stage_s1.go has a test_programs/s1 directory.)
+// This function reads all the files in the test_programs/<<stage_id>> directory and returns their contents as a slice of strings.
+func GetTestProgramsForCurrentStage() []string {
+	var testPrograms []string
+
+	// Get caller info for the caller of this function
+	_, file, no, ok := runtime.Caller(1)
+	if !ok {
+		panic(fmt.Sprintf("CodeCrafters Internal Error: Encountered error while getting caller info: %s#%d", file, no))
+	}
+
+	parentDir := filepath.Dir(file)
+	stageIdentifier := strings.Split(strings.Split(file, ".")[0], "_")[1]
+	testDir := filepath.Join(parentDir, "test_programs", stageIdentifier)
+	files, err := os.ReadDir(testDir)
+	if err != nil {
+		panic(fmt.Sprintf("CodeCrafters Internal Error: Encountered error while reading test directory: %s", err))
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(testDir, file.Name())
+		contents, err := os.ReadFile(filePath)
+		if err != nil {
+			panic(fmt.Sprintf("CodeCrafters Internal Error: Encountered error while reading test file: %s", err))
+		}
+		testPrograms = append(testPrograms, string(contents))
+	}
+	return testPrograms
 }
