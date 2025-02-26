f8598d53bdb75ba22decd27be5372cac454dcb38
Ryan Gang <ryan-gg@outlook.com>
2025-02-26 17:12:07 +0530

    feat: add semantic error handling utilities
    - Create semantic_error.go with error reporting functions
    - Introduce PrintSemanticError for reporting errors to stderr
    - Add MakeSemanticError for creating semantic error instances
    - Include HadSemanticError flag to track semantic error state

---
+++ internal/lox/semantic_error.go
@@ -0,0 +1,20 @@
+package lox
+
+import (
+	"fmt"
+	"os"
+)
+
+// PrintSemanticError reports a semantic error
+func PrintSemanticError(message string) {
+	fmt.Fprintf(os.Stderr, "%v\n", message)
+	HadSemanticError = true
+}
+
+// MakeSemanticError creates a new semantic error
+func MakeSemanticError(message string) error {
+	return fmt.Errorf("%s", message)
+}
+
+// HadSemanticError is true if an evaluation error was encountered
+var HadSemanticError = false
