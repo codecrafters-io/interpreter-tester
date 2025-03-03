68fdd58932bebbfa1b92d202b3ac13c677fbc7ff
Ryan Gang <ryan-gg@outlook.com>
2025-03-03 13:51:15 +0530

    refactor: update type annotations and improve method readability in class implementation
    Modernize type handling and improve code clarity:
    - Replace `interface{}` with `any` type
    - Simplify variable naming in method conditions (e.g., `prs` to `ok`)
    - Add more descriptive comment for UserClass type

--- internal/lox/class.go
+++ internal/lox/class.go
@@ -5,7 +5,7 @@ import (
 	"io"
 )
 
-// Class ...
+// Class is a user defined class
 type UserClass struct {
 	Callable
 	Name    string
@@ -17,9 +17,9 @@ func (c *UserClass) String() string {
 }
 
 // Call is the operation that executes a class constructor
-func (c *UserClass) Call(arguments []interface{}, globalEnv *Environment, stdout io.Writer, stderr io.Writer) (interface{}, error) {
-	instance := &UserClassInstance{Class: c, fields: make(map[string]interface{})}
-	if initializer, prs := c.Methods["init"]; prs {
+func (c *UserClass) Call(arguments []any, globalEnv *Environment, stdout io.Writer, stderr io.Writer) (any, error) {
+	instance := &UserClassInstance{Class: c, fields: make(map[string]any)}
+	if initializer, ok := c.Methods["init"]; ok {
 		_, err := initializer.Bind(instance).Call(arguments, globalEnv, stdout, stderr)
 		if err != nil {
 			return nil, err
@@ -32,7 +32,7 @@ func (c *UserClass) Call(arguments []interface{}, globalEnv *Environment, stdout
 // Arity returns the number of allowed parameters in the class constructor
 // which is always 0
 func (c *UserClass) Arity() int {
-	if initializer, prs := c.Methods["init"]; prs {
+	if initializer, ok := c.Methods["init"]; ok {
 		return initializer.Arity()
 	}
 	return 0
@@ -41,7 +41,7 @@ func (c *UserClass) Arity() int {
 // UserClassInstance is a user defined class instance
 type UserClassInstance struct {
 	Class  *UserClass
-	fields map[string]interface{}
+	fields map[string]any
 }
 
 func (c *UserClassInstance) String() string {
@@ -49,19 +49,19 @@ func (c *UserClassInstance) String() string {
 }
 
 // Get accesses the property
-func (c *UserClassInstance) Get(name Token) (interface{}, error) {
-	if v, prs := c.fields[name.Lexeme]; prs {
+func (c *UserClassInstance) Get(name Token) (any, error) {
+	if v, ok := c.fields[name.Lexeme]; ok {
 		return v, nil
 	}
 
-	if m, prs := c.Class.Methods[name.Lexeme]; prs {
+	if m, ok := c.Class.Methods[name.Lexeme]; ok {
 		return m.Bind(c), nil
 	}
 	return nil, MakeRuntimeError(name, fmt.Sprintf("Undefined property '%s'", name.Lexeme))
 }
 
 // Set accesses the property
-func (c *UserClassInstance) Set(name Token, value interface{}) (interface{}, error) {
+func (c *UserClassInstance) Set(name Token, value any) (any, error) {
 	c.fields[name.Lexeme] = value
 	return nil, nil
 }
